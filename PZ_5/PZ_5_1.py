48821a0feb8116ee4c2afcbbcb4a1a0ebdf611a7
SagidKamilov <89615854074a@gmail.com>
2022-11-06 12:21:56 +0300

    Добавил файлы в PZ_5

---
+++ PZ_5/PZ_5_1.py
@@ -0,0 +1,22 @@
+import random
+
+
+def generate():
+   number_in = random.randint(1000, 9999)  # Генерация случайного числа в диапазоне от 1000 до 9999
+   # Блок с делением числа на цифры
+   num1 = number_in // 1000
+   num2 = number_in // 100 % 10
+   num3 = number_in % 100 // 10
+   num4 = number_in % 10
+   check = 1
+   # Ветки условий, в которых проверяются количество повторяющихся цифр
+   if (num1 == num2) or (num1 == num3) or (num1 == num4):
+       check += 1
+   elif (num2 == num3) or (num2 == num4):
+       check += 1
+   elif num3 == num4:
+       check += 1
+   return 'Количество одинаковых цифр ' + str(check)
+
+
+print(generate())
